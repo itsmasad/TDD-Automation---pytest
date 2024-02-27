2595bb56f6a83f8804712db1720980b7d7cf5174
itsmasad <muhammadasadofficial@gmail.com>
2024-02-27 16:14:02 +0500

    updated code

--- conftest.py
+++ conftest.py
@@ -4,3 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setUp():
+    print("launch browser")
